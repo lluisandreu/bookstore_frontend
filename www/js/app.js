f89c1c778410edf327097c39eaba4d27c8de26ab
Lluís Andreu Oliver Obrador <oliverobrador@gmail.com>
2016-06-01 12:45:30 +0100

    Building the books list and detailed list

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic']);
+var app = angular.module('starter', ['ionic', 'webStorageModule']);
 
 app.run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
@@ -34,45 +34,107 @@ app.config(function ($stateProvider, $urlRouterProvider) {
             url: '/home',
             templateUrl: 'templates/home.html',
             controller: 'MainCtrl'
+        })
+        .state('books', {
+            url: '/books',
+            templateUrl: 'templates/books.html',
+            controller: 'MainCtrl'
+        })
+        .state('book', {
+            url: '/book/:bookId',
+            templateUrl: 'templates/book.html',
+            controller: 'BookCtrl'
         });
     $urlRouterProvider.otherwise('/');
 
 });
 
+app.controller('LoginCtrl',
+
+    function ($ionicSideMenuDelegate, $scope, $http, $state, webStorage) {
+        var logged = webStorage.get('login');
+        if (logged == "logged") {
+            $state.go("home");
+        } else {
+            var loginUrl = "http://multimedia.uoc.edu/frontend/auth.php";
+            $ionicSideMenuDelegate.canDragContent(false);
+            $scope.message = {};
+            $scope.user = {};
+            var cart = [];
+            $scope.login = function (data) {
+                var userLowercase = data.user.toLowerCase();
+                $scope.user = $.param({
+                    user: userLowercase,
+                    passwd: data.passwd,
+                });
+                var config = {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                    }
+                }
+                $http.post(loginUrl, $scope.user, config).then(
+                    function (resp) {
+                        console.log(resp.data);
+                        $scope.status = resp.data.status;
+                        if ($scope.status == 'OK') {
+                            webStorage.set('username', data.user);
+                            webStorage.set('login', 'logged');
+                            webStorage.set('cart', cart);
+                            $state.go("home");
+                        } else {
+                            $scope.message.login = "Your login failed. Please try again";
+                        }
+                    });
+                console.log($scope.user);
+            }
+        }
+    }
+
+
+)
+
 app.controller('MainCtrl',
 
-    function ($http, $scope) {
-        var backendUrl = "http://www.bookstore.backend.boom/index.php/book/rest/";
-        $scope.books = [];
+    function ($ionicSideMenuDelegate, $http, $scope, webStorage) {
+        var backendUrl = "http://eimtcms.uoc.edu/~lluisandreu/mybooks_backend/public_html/index.php/book/rest/";
+        $ionicSideMenuDelegate.canDragContent(true);
+        $scope.books = {};
+        $scope.user = webStorage.get('username');
         $http.get(backendUrl + 'all').then(function (response) {
             $scope.books = response.data;
             console.log(response.data);
         }, function (errResponse) {
             console.error("Can't fetch ".backendUrl);
         });
+        $scope.logout = function () {
+            webStorage.clear('login');
+        }
     }
 );
 
-app.controller('LoginCtrl',
+app.controller('BookCtrl',
 
-    function ($scope, $http, $state) {
-        var loginUrl = "http://multimedia.uoc.edu/frontend/auth.php";
-        $scope.user = {};
-        $scope.login = function (data) {
-            $scope.user.user = data.user;
-            $scope.user.passwd = data.passwd;
-            $http.post(loginUrl, $scope.user).then(
-                function (resp) {
-                    console.log(resp.data);
-                    $scope.status = resp.data.status;
-                    if ($scope.status == 'KO') {
-                        $state.go("home");
-                    }
-                });
-            console.log($scope.user);
+    function ($ionicSideMenuDelegate, $stateParams, $http, $scope, webStorage) {
+        $ionicSideMenuDelegate.canDragContent(true);
+        var backendUrl = "http://eimtcms.uoc.edu/~lluisandreu/mybooks_backend/public_html/index.php/book/rest/";
+        var bookId = $stateParams.bookId;
+        $http.get(backendUrl + bookId).then(function (response) {
+            $scope.books = response.data;
+            console.log($scope.books);
+        }, function (errResponse) {
+            console.error("Can't fetch ".backendUrl);
+        });
+        $scope.addToCard = function (id) {
+            var order = webStorage.get('cart');
+            order.push(id);
+            webStorage.set('cart', order);
         }
+        console.log(webStorage.get('cart'));
     }
-)
+);
+
+
+
 
 function ContentController($scope, $ionicSideMenuDelegate) {
     $scope.toggleLeft = function () {
